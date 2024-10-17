7af5bc54316da276527b417fc2b6c3dd3ced20a9
Aaron Padree <aaronbpadre-e@iskolarngbayan.pup.edu.ph>
2024-10-17 10:44:23 +0800

    Version 17

--- core/settings.py
+++ core/settings.py
@@ -49,15 +49,16 @@ INSTALLED_APPS = [
 
 
 MIDDLEWARE = [
-    'whitenoise.middleware.WhiteNoiseMiddleware',  # Move WhiteNoise to the top
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'homepage.middleware.CustomLoginRequiredMiddleware',  # Ensure this remains after AuthenticationMiddleware
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    
+    'homepage.middleware.CustomLoginRequiredMiddleware',  # Correct path to your custom middleware
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 
